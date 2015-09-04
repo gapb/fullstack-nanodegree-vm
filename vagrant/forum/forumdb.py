e88831dfb1c93d98461889b340d8b55b4bb44e82
Gilbert Podell-Blume <gilbertpodell-blume@Peregrin-II.local>
2015-09-04 16:32:35 -0600

    Began work on tournament.sql.

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,11 +3,12 @@
 # 
 
 import time
+import psycopg2
 
-## Database connection
-DB = []
+# Database connection
+# DB = psycopg2.connect("dbname=forum")
 
-## Get posts from database.
+# Get posts from database.
 def GetAllPosts():
     '''Get all the posts from the database, sorted with the newest first.
 
@@ -16,16 +17,28 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    DB = psycopg2.connect("dbname=forum")
+    curs = DB.cursor()
+    curs.execute("SELECT * FROM posts ORDER BY time DESC")
+    posts = [{'content': str(row[0]), 'time': str(row[1])} for row in
+             curs.fetchall()]
+    # posts.sort(key=lambda row: row['time'], reverse=True)
+    curs.close()
+    DB.close()
     return posts
 
-## Add a post to the database.
+# Add a post to the database.
 def AddPost(content):
     '''Add a new post to the database.
 
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    DB = psycopg2.connect("dbname=forum")
+    curs = DB.cursor()
+    curs.execute("INSERT INTO posts (content) VALUES (%s)", (content,))
+    DB.commit()
+    curs.close()
+    DB.close()
+    # t = time.strftime('%c', time.localtime())
+    # DB.append((t, content))
