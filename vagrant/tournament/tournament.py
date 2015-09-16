f7eff2dc77e7ff34d49580f83a5dc204e107b666
Gilbert Podell-Blume <gilbertpodell-blume@Peregrin-II.local>
2015-09-16 14:36:40 -0600

    Committing before I completely change direction on the swissPairing() function.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -4,6 +4,7 @@
 #
 
 import psycopg2
+from random import choice
 
 
 def connect():
@@ -39,15 +40,21 @@ def countPlayers():
     """Returns the number of players currently registered."""
     db = connect()
     curs = db.cursor()
-    curs.execute("SELECT id FROM players")
-    # todo: see if I can run len() on curs()
-    # todo: alternately, do this with a query using count(*)
-    count = 0
-    for playerID in curs.fetchall():
-        ++count
-    curs.close()
-    db.close()
+    curs.execute("SELECT COUNT(*) FROM players")
+    result = curs.fetchone()
+
+    count = 0 if result is None else result[0]
+
+    # curs.execute("SELECT id FROM players")
+    # # todo: see if I can run len() on curs()
+    # # todo: alternately, do this with a query using count(*)
+    # count = 0
+    # for playerID in curs.fetchall():
+    #     ++count
+    # curs.close()
+    # db.close()
     return count
+    # todo: add error handling
 
 
 def registerPlayer(name):
@@ -81,6 +88,13 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    db = connect()
+    curs = db.cursor()
+    curs.execute("SELECT * FROM players ORDER BY wins DESC")
+    standings = curs.fetchall()
+    curs.close()
+    db.close()
+    return standings
 
 
 def reportMatch(winner, loser):
@@ -90,6 +104,21 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
+    # TODO add error handling
+    # TODO accommodate ties?
+    db = connect()
+    curs = db.curs()
+    # update matches table
+    curs.execute("INSERT INTO matches (winnerID, loserID) VALUES (%s, %s)",
+                 (winner, loser))
+    # update matches count
+    curs.execute("UPDATE players SET matches = matches + 1 WHERE id = %s OR "
+                 "id = %s", (winner, loser))
+    # update winner
+    curs.execute("UPDATE players SET wins = wins + 1 WHERE id = %s", (winner,))
+    db.commit()
+    curs.close()
+    db.close()
  
  
 def swissPairings():
@@ -107,5 +136,73 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    db = connect()
+    curs = db.cursor()
+    # Step 1: grab all players.
+    # Tuple Order: (ID, name, Wins, Matches, name)
+    curs.execute("SELECT id, name, wins, matches FROM players ORDER BY wins, "
+                 "matches DESC")
+
+    # First, let's check for the case where no matches have been played yet.
+    # We can identify this case by fetching the first tuple, and checking if
+    # wins == matches == 0
+    players = []
+    first_player = curs.fetchone()
+    # None check because why not
+    if first_player is None:
+        return None
+    if first_player[2] == 0 and first_player[3] == 0:
+        players.append(first_player)
+        # also insert everything else into the players list.
+        players.extend(curs)
+        # create list for pairings
+        pairings = []
+        # randomly generate pairings, assuming len(players) is even
+        while len(pairings) != 0:
+            player1 = choice(players)
+            players.remove(player1)
+            player2 = choice(players)
+            players.remove(player2)
+            # todo make more efficient?
+            pairings.append((player1, player2))
+        return pairings
+    # Now, if matches have been played, players will need to be a list of lists
+    else:
+        player_group_id = 0
+        players.append([first_player])
+        next_player = curs.fetchone()
+        while next_player is not None:
+            # if numWins is different, we've begun a new group
+            if next_player[2] != players[player_group_id][2]:
+                players.append([next_player])
+                ++player_group_id
+            else:
+                players[player_group_id].append(next_player)
+            next_player = curs.fetchone()
+        # Now, having gotten players grouped by numWins, it's time to generate
+        # pairings.
+        player_group_id = 0
+        pairings = []
+        while player_group_id < len(players):
+            player_group = players[player_group_id]
+            # choose player1
+            player1 = choice(player_group)
+            player_group.remove(player1)
+            # choose player2. Assumes subgroups are even—not safe
+            if len(player_group) == 0:
+                player_group = players[++player_group_id]
+            player2 = choice(player_group)
+            player_group.remove(player2)
+            if len(player_group) == 0:
+                ++player_group_id
+            pairings.append((player1, player2))
+            # TODO check for duplicates
+        return pairings
+
+
+
+
+
+
 
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -14,26 +14,18 @@ CREATE TABLE players (
   matches INTEGER DEFAULT 0
 );
 
--- I'm not sure if this wholly necessary.
--- matches table prevents rematches by asserting player1 and player2 as
--- primary keys
+-- Table for storing match results. TODO accommodate ties?
 CREATE TABLE matches (
-  player1ID INTEGER,
-  player2ID INTEGER,
   winnerID INTEGER,
-  PRIMARY KEY (player1ID, player2ID),
-  FOREIGN KEY (player1ID) REFERENCES players(id),
-  FOREIGN KEY (player2ID) REFERENCES players(id),
+  loserID INTEGER,
+  PRIMARY KEY (winnerID, loserID),
   FOREIGN KEY (winnerID) REFERENCES players(id),
-  CHECK (player1ID < player2ID)
+  FOREIGN KEY (loserID) REFERENCES players(id)
 );
 
 CREATE INDEX playersByWins
 ON players(wins, matches, id);
 
-CREATE INDEX matchesByP2
-ON matches(player2ID, player1ID);
-
-CREATE INDEX matchesByWinner
-ON matches(winnerID, player1ID, player2ID);
+CREATE INDEX matchesByLoser
+ON matches(loserID, winnerID);
 
